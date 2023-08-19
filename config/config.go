e99685ee1b5a99aa995c751c72670bba03e3a1c9
KevinHansaWardhana <kevinhansawardhana@rocketmail.com>
2023-08-19 08:32:50 +0700

    pertama commit

---
+++ config/config.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"prakerja/structs"
+
+	"github.com/jinzhu/gorm"
+)
+
+// DBInit create connection to database
+func DBInit() *gorm.DB {
+	db, err := gorm.Open("mysql", "root:NR1gnsGxsJ9jz1ljgghh@tcp(containers-us-west-195.railway.app:7048)/railway?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic("failed to connect to database")
+	}
+
+	db.AutoMigrate(structs.Person{})
+	return db
+}
