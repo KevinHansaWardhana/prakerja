16b2e3654ded1e07c2dc4a5f5b57a21eef7e0a4f
KevinHansaWardhana <kevinhansawardhana@rocketmail.com>
2023-08-19 10:23:16 +0700

    commit config

--- config/config.go
+++ config/config.go
@@ -8,7 +8,7 @@ import (
 
 // DBInit create connection to database
 func DBInit() *gorm.DB {
-	db, err := gorm.Open("mysql", "root:NR1gnsGxsJ9jz1ljgghh@tcp(containers-us-west-195.railway.app:7048)/railway?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", "root:NR1gnsGxsJ9jz1ljgghh@tcp(prakerja-production-f6dd.up.railway.app:7048)/railway?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic("failed to connect to database")
 	}
